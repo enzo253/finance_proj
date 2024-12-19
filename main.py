87e0d570e5b16b9bac290f06fbcf878ab40415a4
enzo253 <139024939+enzo253@users.noreply.github.com>
2024-12-19 14:36:43 +0100

    new save

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas 
+import numpy
+import tkinter as tk
+from tkinterdnd2 import DND_FILES, TkinterDnD
+
+def handle_file_drop(your_file):
+    file_path = your_file.data.strip()
+    try:
+        finance_report = pd.read_csv(file_path)
+        print(finance_report.head())
+            return: 'report uploaded succesfully'
+    execpt Exceptions as e
+    return f'unseccesfull: {e}
+
+    
